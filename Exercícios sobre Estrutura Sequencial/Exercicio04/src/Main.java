9540310f9049ad76fe5cbe23ed0dfc26c9325379
Leandro <leandrogerolim@gmail.com>
2022-11-27 11:46:30 -0400

    Exercício 04 concluído.

--- Exercícios sobre Estrutura Sequencial/Exercicio04/src/Main.java
+++ Exercícios sobre Estrutura Sequencial/Exercicio04/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Informe o número do funcionario, o total de horas e o valor de cada hora");
+        Locale.setDefault(Locale.US);
+      Scanner sc = new Scanner(System.in);
+
+      int funcionario = sc.nextInt();
+      int horasTotal = sc.nextInt();
+      double valorHora = sc.nextDouble();
+
+      double salario = horasTotal * valorHora;
+
+        System.out.println("NUMBER = " + funcionario);
+        System.out.printf("SALARY = U$ %.2f",salario);
     }
 }
