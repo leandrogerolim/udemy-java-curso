3fc7f196a9f13bc47c209a114f6a8ba5e14148c3
Leandro <leandrogerolim@gmail.com>
2022-11-27 11:15:16 -0400

    Exercicio 03 concluído.

--- Exercícios sobre Estrutura Sequencial/Exercicio03/src/Main.java
+++ Exercícios sobre Estrutura Sequencial/Exercicio03/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("informe 4 valores: ");
+
+        int valorA = sc.nextInt();
+        int valorB = sc.nextInt();
+        int valorC = sc.nextInt();
+        int valorD = sc.nextInt();
+
+        int valorTotal = (valorA * valorB) - (valorC * valorD);
+
+        System.out.println("DIFERENÇA = " + valorTotal);
     }
 }
