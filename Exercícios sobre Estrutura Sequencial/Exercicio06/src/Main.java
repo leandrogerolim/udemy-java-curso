0ad3d3caddaf0a8e21d48380a2596751aa05afcd
Leandro <leandrogerolim@gmail.com>
2022-11-28 10:10:05 -0400

    Exercício 06 concluído.

--- Exercícios sobre Estrutura Sequencial/Exercicio06/src/Main.java
+++ Exercícios sobre Estrutura Sequencial/Exercicio06/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        double valorA = sc.nextDouble();
+        double valorB = sc.nextDouble();
+        double valorC = sc.nextDouble();
+
+        double triangulo = valorA * valorC / 2;
+        double circulo =  3.14159 * (valorC * valorC);
+        double trapezio = (valorC * (valorA + valorB)) / 2;
+        double quadrado = valorB * valorB ;
+       double retangulo = valorA * valorB ;
+
+        System.out.printf("TRIANGULO: %.3f%n",triangulo);
+        System.out.printf("CÍRCULO: %.3f%n",circulo);
+        System.out.printf("TRAPÉZIO: %.3f%n",trapezio);
+        System.out.printf("QUADRADO: %.3f%n",quadrado);
+        System.out.printf("RETANGULO: %.3f%n",retangulo);
+
     }
 }
