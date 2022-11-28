7cc65665e79704f18692464e32446001ceb24e12
Leandro <leandrogerolim@gmail.com>
2022-11-28 09:29:07 -0400

    Exercício 05 concluído.

--- Exercícios sobre Estrutura Sequencial/Exercicio05/src/Main.java
+++ Exercícios sobre Estrutura Sequencial/Exercicio05/src/Main.java
@@ -9,7 +9,17 @@ public class Main {
         int item1 = sc.nextInt();
         int quantidade1 = sc.nextInt();
         double valor1 = sc.nextDouble();
-        System.out.println("Proxímo item, digite o codigo, quantidade e valor unitário: ");
+        double total1 = quantidade1 * valor1;
+        System.out.println("Proxímo item, digite o código, quantidade e valor unitário: ");
+
+        int item2 = sc.nextInt();
+        int quantidade2 = sc.nextInt();
+        double valor2 = sc.nextDouble();
+        double total2 = quantidade2 * valor2;
+
+        double valorTotal = total1 + total2;
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f" , valorTotal);
 
 
     }
