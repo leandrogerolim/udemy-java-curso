6969a5d76047d2409ab29b72b91311dbb306c8e1
Leandro <leandrogerolim@gmail.com>
2022-11-28 09:20:08 -0400

    Exercício 05 criado.

---
+++ Exercícios sobre Estrutura Sequencial/Exercicio05/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int item1 = sc.nextInt();
+        int quantidade1 = sc.nextInt();
+        double valor1 = sc.nextDouble();
+        System.out.println("Proxímo item, digite o codigo, quantidade e valor unitário: ");
+
+
+    }
+}
