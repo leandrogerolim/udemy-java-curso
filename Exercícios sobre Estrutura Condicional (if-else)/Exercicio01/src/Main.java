dcb663594cd58677b640a618ba3002d97beecb4f
Leandro <leandrogerolim@gmail.com>
2022-11-30 19:17:03 -0400

    Exercício 01 concluído.

--- Exercícios sobre Estrutura Condicional (if-else)/Exercicio01/src/Main.java
+++ Exercícios sobre Estrutura Condicional (if-else)/Exercicio01/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        int valorInformado = sc.nextInt();
+
+        if(valorInformado >= 0){
+            System.out.println("NÃO NEGATIVO");
+
+        }else{
+            System.out.println("NEGATIVO");
+        }
+
     }
 }
