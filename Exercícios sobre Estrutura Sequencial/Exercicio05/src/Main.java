5836ae1919767c9b8efaec9a8a033afbc15fd871
Leandro <leandrogerolim@gmail.com>
2022-11-29 22:41:44 -0400

    Exercício 05 concluído.

--- Exercícios sobre Estrutura Sequencial/Exercicio05/src/Main.java
+++ Exercícios sobre Estrutura Sequencial/Exercicio05/src/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
+
+
         Scanner sc = new Scanner(System.in);
 
         int item1 = sc.nextInt();
