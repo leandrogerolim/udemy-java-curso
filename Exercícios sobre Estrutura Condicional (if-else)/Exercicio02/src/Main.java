5ba30cbc8cdf0eaf95819b141a968df677f68b56
Leandro <leandrogerolim@gmail.com>
2022-11-30 19:38:25 -0400

    Exercício 02 concluído.

--- Exercícios sobre Estrutura Condicional (if-else)/Exercicio02/src/Main.java
+++ Exercícios sobre Estrutura Condicional (if-else)/Exercicio02/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        int valorInformado = sc.nextInt();
+
+        int valor = valorInformado % 2;
+
+        if (valor ==  0) {
+            System.out.println("PAR!");
+        }else {
+                System.out.println("IMPAR!");
     }
+}
 }
