cb2fdc5d44ad65dc22f0077e7d50aa31e33f0ac8
Leandro <leandrogerolim@gmail.com>
2022-11-30 22:48:40 -0400

    Exercício 03 concluído.

--- Exercícios sobre Estrutura Condicional (if-else)/Exercicio03/src/Main.java
+++ Exercícios sobre Estrutura Condicional (if-else)/Exercicio03/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        int valorA = sc.nextInt();
+        int valorB = sc.nextInt();
+        int aux;
+        int auxB;
+        int valorMultiplo = valorB % valorA;
+        if (valorMultiplo == 0) {
+
+            System.out.println("São Múltiplos");
+
+
+        } else if (valorA > valorB) {
+            aux = valorB;
+            auxB = valorA;
+            valorMultiplo = auxB % aux;
+
+            if (valorMultiplo == 0) {
+                System.out.println("São Múltiplos");
+            }else if (valorMultiplo != 0) {
+                System.out.println("Não são Múltiplos");
+
+
+            }
+            }else{
+            System.out.println("Não são Múltiplos");
+        }
     }
 }
