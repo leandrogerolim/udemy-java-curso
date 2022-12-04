fea12da760ecad6738b1cbd1ffff7d1a8ff73a3f
Leandro <leandrogerolim@gmail.com>
2022-12-04 12:58:02 -0400

    Exercicío 06 concluído.

--- Exercícios sobre Estrutura Condicional (if-else)/Exercicio06/src/Main.java
+++ Exercícios sobre Estrutura Condicional (if-else)/Exercicio06/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale .setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double valorIntervalo = sc.nextDouble();
+
+        if (valorIntervalo >= 0.00 && valorIntervalo <= 25.00) {
+            System.out.println("Intervalo (0,25)");
+
+        }else if (valorIntervalo >= 25.01 && valorIntervalo <= 50.00) {
+                System.out.println("Intervalo (25,50)");
+
+        }else if (valorIntervalo >= 50.01 && valorIntervalo <= 75.00) {
+            System.out.println("Intervalo (50,75)");
+
+        }else if (valorIntervalo >= 75.01 && valorIntervalo <= 100.00) {
+            System.out.println("Intervalo (75,100)");
+
+        }else {
+            System.out.println("Fora do Intervalo");
+        }
+
+
     }
 }
