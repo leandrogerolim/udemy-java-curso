e7943487b3255beac14815db5bb5ef3a9d6e83c6
Leandro <leandrogerolim@gmail.com>
2022-12-05 11:09:42 -0400

    Exercicío 08 concluído.

--- Exercícios sobre Estrutura Condicional (if-else)/Exercicio08/src/Main.java
+++ Exercícios sobre Estrutura Condicional (if-else)/Exercicio08/src/Main.java
@@ -1,6 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        Double salario = sc.nextDouble();
+        Double novoSalario;
+
+        if (salario <= 2000.00) {
+            novoSalario = 00.00;
+
+
+        } else if (salario <= 3000.00) {
+            novoSalario = salario- 2000.00;
+            novoSalario =  novoSalario * 0.08  ;
+
+        } else if ( salario <= 4500.00) {
+            novoSalario = salario - 3000.00;
+            novoSalario = novoSalario * 0.18;
+            novoSalario = novoSalario + 1000 * 0.08;
+        }else {
+            novoSalario = (salario - 4500.00) * 0.28 + 1500.0 * 0.18 + 1000.0 * 0.08;
+        }
+            if (novoSalario == 0.000){
+                System.out.println(("Isento"));
+            }else {
+                System.out.printf("R$ %.2f",novoSalario);
+            }
+
+       }
     }
-}
